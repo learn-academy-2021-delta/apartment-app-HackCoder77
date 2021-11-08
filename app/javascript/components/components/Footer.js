078abf5ef32bb4034cbffa0bd012b467c5a682b5
hackcoder77 <hackcoder77@gmail.com>
2021-11-08 11:59:57 -0800

    landing page finally complete

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
     render() {
         return (
             <>
-            <h3>This is the footer</h3>
+            <h3>This is the footer of foots</h3>
             </>
         )
     }
